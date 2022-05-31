ae0b93ef3ac1b37a3d183236cf1d2916cc33ba00
Gowtamkumar <gowtampaul0@gmail.com>
2022-05-31 20:55:39 +0600

    not Found

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Sidebar3 from './view/Sidebar3';
 import Sidebar4 from './view/Sidebar4';
 import Dashboard from './view/Dashboard';
 import Setting from './view/setting';
+import NotFound from './view/NotFound';
 
 
 
@@ -20,6 +21,7 @@ function App() {
     <React.Suspense fallback={loading()} >
       <Routes>
         <Route path="/login" name="Login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="dashboard" name="dashboard" element={<IndexLayoutd />} >
           <Route path="/dashboard" name="dashboard" element={<Dashboard />} />
           <Route path="sidebar1" name="dashboard" element={<Sidebar1 />} />
@@ -27,6 +29,7 @@ function App() {
           <Route path="sidebar3" name="dashboard" element={<Sidebar3 />} />
           <Route path="sidebar4" name="dashboard" element={<Sidebar4 />} />
           <Route path="setting" name="dashboard" element={<Setting />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </React.Suspense>
